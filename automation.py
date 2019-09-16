65ecec9506ef2084daffbb47d7d564055cdfa916
butch <butchpaolom@gmail.com>
2019-09-16 23:23:59 +0800

    mod from rpi

--- automation.py
+++ automation.py
@@ -1,6 +1,7 @@
 import sys, os
 import django
 import time
+import RPi.GPIO as rl
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ecobrick.settings")
 django.setup()
 
@@ -8,12 +9,41 @@ from eb.models import *
 
 #model is 'Setting'
 #automation for ecobrick is written here, from settings.
+rl.setmode(rl.BOARD)
+rl.setup(33,rl.OUT)
+rl.setup(35,rl.OUT)
+rl.setup(37,rl.OUT)
+rl.setup(31,rl.OUT)
+rl.setup(33,rl.HIGH)
+rl.setup(35,rl.HIGH)
+rl.setup(37,rl.HIGH)
+rl.setup(31,rl.HIGH)
 
 
 while (True):
     try:
-        x = Setting.objects.get(setting_name='machine_switch')
+        ms = Setting.objects.get(setting_name='machine_switch')
     except:
-        x = None
-    print (x)
+        ms = None
+    try:
+        ct = Setting.objects.get(setting_name='compressor_trigger')
+    except:
+        ct = None
+    try:
+        bs = Setting.objects.get(setting_name='bottle_size')
+    except:
+        bs = None
+    if ms.value==1:
+        rl.setup(33,rl.LOW)
+        rl.setup(35,rl.LOW)
+        rl.setup(37,rl.LOW)
+        rl.setup(31,rl.LOW)
+    else:
+        rl.setup(33,rl.HIGH)
+        rl.setup(35,rl.HIGH)
+        rl.setup(37,rl.HIGH)
+        rl.setup(31,rl.HIGH)
+    print (ms)
+    print (ct)
+    print (bs)
     time.sleep(.3)
