057f451e21971cea0b5e138b4a5f20aba89f8370
unknown <butchpaolom@gmail.com>
2019-09-18 14:25:08 +0800

    test oop

--- automation.py
+++ automation.py
@@ -15,15 +15,16 @@ wiper = 33      #pin 33 wiper
 shredder = 35   #pin 35 shredder 
 extra_pin = 37
 
-# rl.setup(compressor,rl.OUT)
-# rl.setup(wiper,rl.OUT) 
-# rl.setup(shredder,rl.OUT) 
-# rl.setup(extra_pin,rl.OUT) 
+rl.setmode(rl.BCM)
+rl.setup(compressor,rl.OUT)
+rl.setup(wiper,rl.OUT) 
+rl.setup(shredder,rl.OUT) 
+rl.setup(extra_pin,rl.OUT) 
 
-# rl.setup(compressor,rl.HIGH)
-# rl.setup(wiper,rl.HIGH)
-# rl.setup(shredder,rl.HIGH)
-# rl.setup(extra_pin,rl.HIGH)
+rl.setup(compressor,rl.HIGH)
+rl.setup(wiper,rl.HIGH)
+rl.setup(shredder,rl.HIGH)
+rl.setup(extra_pin,rl.HIGH)
 
 seconds = 0
 
